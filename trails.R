4b77a0d7adacbed38d3552ad142d841d8e1ac781
Joshua Rosenberg <jrosen@msu.edu>
2021-11-14 16:00:28 -0500

    update trails, index, why

--- trails.R
+++ trails.R
@@ -5,6 +5,10 @@
 library(ggmap)
 library(sf)
 library(tidyverse)
+library(ggmap)
+library(osmdata)
+library(elevatr)
+library(geosphere)
 
 # Open files
 
@@ -13,6 +17,19 @@ f <-
 
 shp <- st_read(f)
 
+name <- "Alum Cave"
+
+my_trail_id <- shp %>% 
+    filter(str_detect(TRAILNAME, name)) %>% 
+    pull(OBJECTID)
+
+my_trail_id
+
+my_trail_id <- shp[my_trail_id, ] %>% 
+    arrange(desc(Shape__Len)) %>% 
+    slice(1) %>% 
+    pull(OBJECTID) # gets the longest segment, for now
+
 coords <-
     st_coordinates(shp) %>% 
     as_data_frame()
@@ -20,56 +37,118 @@ coords <-
 # Let's try it with the first trail
 
 t1 <- coords %>% 
-    filter(L1 == 203)
+    filter(L1 %in% my_trail_id)
+
+start = as.matrix(t1[1, c(1, 2)]) # first row
+
+random_point <- as.matrix(t1[1043, c(1, 2)])
+
+all_points <- t1[, c(1, 2)]
+
+dist_matrix <- distm(all_points) %>% 
+    as_tibble()
+
+dist_matrix_proc <- dist_matrix %>% 
+    mutate(row = row_number()) %>% 
+    gather(key, val, -row) %>% 
+    mutate(key = str_sub(key, 2)) %>% 
+    mutate(key = as.numeric(key)) %>% 
+    filter(key == row + 1 | (row == 1 & key == 1))
+
+dist_matrix_final <- dist_matrix_proc %>% 
+    rename(distance_m = val) %>% 
+    mutate(distance_ft = distance_m * 3.28084) %>% 
+    mutate(cumulative_distance_ft = cumsum(distance_ft)) %>% 
+    mutate(cumulative_distance_mi = cumulative_distance_ft/5280)
+
+mile_markers <- dist_matrix_final %>% 
+    mutate(cumulative_distance_mi_trunc = trunc(cumulative_distance_mi)) %>% 
+    mutate(mile_marker = ifelse(lead(cumulative_distance_mi_trunc) > cumulative_distance_mi_trunc, 
+                                lead(cumulative_distance_mi_trunc), 0))
+
+# sf::st_distance(start, random_point)
+
+t_other <- coords %>% 
+    filter(!(L1 %in% my_trail_id))
 
 # Plot
 
 bb <- getbb("")
-bb[1, 1] <- min(t1$X) + 2*(min(t1$X) - max(t1$X))  # x min - how far west
-bb[1, 2] <- max(t1$X) - 2*(min(t1$X) - max(t1$X)) # x max - how far east
-bb[2, 1] <- min(t1$Y) + (min(t1$Y) - max(t1$Y)) # y min - how far south
-bb[2, 2] <- max(t1$Y) - (min(t1$Y) - max(t1$Y))# ymax - how far north
+bb[1, 1] <- min(t1$X) + 1.5*(min(t1$X) - max(t1$X))  # x min - how far west
+bb[1, 2] <- max(t1$X) - 1.5*(min(t1$X) - max(t1$X)) # x max - how far east
+bb[2, 1] <- min(t1$Y) + .65*(min(t1$Y) - max(t1$Y)) # y min - how far south
+bb[2, 2] <- max(t1$Y) - .65*(min(t1$Y) - max(t1$Y))# ymax - how far north
 
 m <- get_stamenmap(bb, maptype = "terrain", zoom = 14)
 
-mt_wash <- data.frame(x = -71.3036, y = 44.2700)
 ll_prj <- "EPSG:4326"
 
-elev <- get_elev_point(locations = as.data.frame(t1[, ]), prj = ll_prj)
+elev <- get_elev_point(locations = as.data.frame(t1), prj = ll_prj)
 
-elev <- elev %>% 
+elev_proc <- elev %>% 
     as_tibble() %>% 
-    mutate(elevation = elevation * 3.28084)
+    mutate(elevation = elevation * 3.28084) %>% 
+    select(elevation)
 
 t1 <- t1 %>% 
-    bind_cols(elev)
-    
-ggmap(m) +
+    bind_cols(elev_proc)
+
+mile_markers <- mile_markers %>% 
+    mutate(mile_marker = ifelse(mile_marker == 0, NA, str_c("Mile: ", mile_marker)))
+
+t1 <- t1 %>%
+    bind_cols(mile_markers)
+
+# for specific trail - place_marker
+
+t1 <- t1 %>% 
+    mutate(place_marker = ifelse(row == 1037, "Mt. LeConte Summit",
+                                 ifelse(row == 465, "Alum Cave", NA)))
+
+t1 <- t1 %>% 
+    mutate(label = ifelse(!is.na(mile_marker), mile_marker,
+                          ifelse(!is.na(place_marker), place_marker,
+                                  ifelse(key == 2, "Start", NA))))
+
+p <- ggmap(m) +
+    geom_point(data = t_other, 
+               aes(x = X,
+                   y = Y),
+               color = "gray90", 
+               alpha = .50,
+               size = .05) +
     geom_point(data = t1, 
                aes(x = X,
                    y = Y,
                    color = elevation), 
-               size = .30) +
-    geom_point(data = slice(t1, 1),
+               size = .40,
+               alpha = .9) +
+    
+    # place markers
+    geom_point(data = filter(t1, !is.na(label)),
                aes(x = X, 
                    y = Y),
-               color = "black") +
-    ggrepel::geom_label_repel(data = slice(t1, 1),
-               aes(x = X, 
-                   y = Y), label = "Start") +
-    geom_point(data = slice(t1, nrow(t1) - 1),
-               aes(x = X, 
-                   y = Y),
-               color = "black") +
-    ggrepel::geom_label_repel(data = slice(t1, nrow(t1) - 1),
+               color = "black",
+               alpha = .65) +
+    ggrepel::geom_label_repel(data = t1,
                               aes(x = X, 
-                                  y = Y), label = "End") +
-    ggtitle("Alum Cave Trail") +
+                                  y = Y,
+                                  label = label),
+                              alpha = .825,
+                              box.padding = .75) +
+    
+    ggtitle(str_c(name, " Trail (", 
+                  mile_markers %>% slice(nrow(.)) %>% pull(cumulative_distance_mi) %>% round(2),
+                  " mi.)")) +
+    labs(subtitle = "Great Smoky Mountains National Park") +
     scale_color_gradient("Elevation (ft.)", 
-                         low = "gray",
-                         high = "orchid4") + 
+                         low = "#dadaeb",
+                         high = "#756bb1") + 
+    # theme(text = element_text(family = "Montserrat")) +
     theme_void()
+
+p
 
 # Save file
 
-ggsave(here::here("output", "boulevard-map.png"), dpi = "retina", width = 8, height = , units = "in")
+ggsave(here::here("output", str_c(tolower(name), ".png")), dpi = "retina", width = 8, height = , units = "in")
