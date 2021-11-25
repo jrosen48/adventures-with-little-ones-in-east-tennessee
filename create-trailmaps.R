9d5fd15e4272e62cbce0a0ad8c9b61a03c6b4d96
Joshua Rosenberg <jrosen@msu.edu>
2021-11-24 23:26:56 -0500

    move code to create plots over to a separate file; clean-up, re-render

---
+++ create-trailmaps.R
@@ -0,0 +1,5 @@
+source("gaia-trails.R")
+
+t <- create_and_save_trailmap(name = "Abrams Falls", 
+                              file_name_1 = "abrams-little-bottoms.gpx",
+                              auxiliary_trail_files = "GRSM_TRAILS.shp")
