0d485c91c0f24dd9db9907418bd59a36e5080f6a
Joshua Rosenberg <jrosen@msu.edu>
2022-01-23 09:54:05 -0500

    re-render index

--- render.R
+++ render.R
@@ -7,6 +7,6 @@ trail_info <- read_sheet("https://docs.google.com/spreadsheets/d/1rsNpzmcLIaBD1f
 trail_info %>% write_csv("trail-info.csv")
 
 sci_info <- read_sheet("https://docs.google.com/spreadsheets/d/1rsNpzmcLIaBD1ftLnXl9Sqn1XERaSZir_rwLszIwrz0/edit#gid=0", sheet = 2)
-trail_info %>% write_csv("sci-info.csv")
+sci_info %>% write_csv("sci-info.csv")
 
 rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
