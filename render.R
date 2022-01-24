c3b698b20a5f5ad7ee941a24f5c1015d23c52b43
Joshua Rosenberg <jrosen@msu.edu>
2022-01-23 21:07:23 -0500

    update front matter

--- render.R
+++ render.R
@@ -9,7 +9,7 @@ trail_info %>% write_csv("trail-info.csv")
 sci_info <- read_sheet("https://docs.google.com/spreadsheets/d/1rsNpzmcLIaBD1ftLnXl9Sqn1XERaSZir_rwLszIwrz0/edit#gid=0", sheet = 2)
 sci_info %>% write_csv("sci-info.csv")
 
-history_info <- read_sheet("https://docs.google.com/spreadsheets/d/1rsNpzmcLIaBD1ftLnXl9Sqn1XERaSZir_rwLszIwrz0/edit#gid=0", sheet = 2)
+history_info <- read_sheet("https://docs.google.com/spreadsheets/d/1rsNpzmcLIaBD1ftLnXl9Sqn1XERaSZir_rwLszIwrz0/edit#gid=0", sheet = 3)
 history_info %>% write_csv("history-info.csv")
 
 rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
