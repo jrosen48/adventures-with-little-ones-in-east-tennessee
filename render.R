e20a8ba7a142a89b5a9f91710f12a04829a7dc8a
Joshua Rosenberg <jrosen@msu.edu>
2022-01-30 21:44:55 -0500

    update

--- render.R
+++ render.R
@@ -1,4 +1,3 @@
-# source("create-all-trail-maps.R")
 library(googlesheets4)
 library(tidyverse)
 
@@ -11,5 +10,7 @@ sci_info %>% write_csv("sci-info.csv")
 
 history_info <- read_sheet("https://docs.google.com/spreadsheets/d/1rsNpzmcLIaBD1ftLnXl9Sqn1XERaSZir_rwLszIwrz0/edit#gid=0", sheet = 3)
 history_info %>% write_csv("history-info.csv")
+
+source("create-all-trail-maps.R")
 
 rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
